011feb6ece5eb4df349edacf25c19fc0110d89fa
LHJ <LHJ@DESKTOP-BHT6G1Q>
2022-08-01 11:19:46 +0900

    aa

---
+++ src/intro/JejuProMain.java
@@ -0,0 +1,13 @@
+package intro;
+
+
+import character.JejuTypeSelection;
+
+public class JejuProMain {
+	public static void main(String[] args) {
+		
+		JejuBase.getInstance(new JejuBegin());
+
+	}
+
+}
